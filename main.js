04e4993443d53ac4866e380ecde98b927a0a28af
Yuzunoha <studysteadystandby@gmail.com>
2019-12-21 17:28:47 +0900

    JSからテーブルの行を追加した

---
+++ main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const tbody = document.getElementById('tbody');
+
+const createRow = (
+  textId, text, textUpdatedAt, userId, userName, userBio, userUpdatedAt
+) => {
+  let s = '';
+  s += '<tr>';
+  s += '<td>' + textId + '</td>';
+  s += '<td>' + text + '</td>';
+  s += '<td><div class="css-fukidashi">';
+  s += '<p class="text">' + userName + '</p>';
+  s += '<p class="fukidashi">';
+  s += 'id: ' + userId + '<br>';
+  s += 'bio: ' + userBio + '<br>';
+  s += 'updated_at: ' + userUpdatedAt;
+  s += '</p>';
+  s += '</div></td>';
+  s += '<td>' + textUpdatedAt + '</td>';
+  s += '</tr>';
+  return s;
+}
+
+const update = () => {
+  const rawStr = createRow(
+    1234,
+    'jsから書いた文章',
+    '2019/12/25',
+    4321,
+    'JS太郎',
+    'JS太郎です',
+    '2019/12/23'
+  );
+  tbody.innerHTML = rawStr;
+};
+
+update();
